4a9d3c08a92309bf0e88b8722bd6ffff6ad843c1
AjayDHOLIA <ajaydholia19@gmail.com>
2022-08-20 15:38:27 +0530

    assignment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyCalendar from "./component/calender";
+
+function App() {
+  return (
+    <div className="App">
+      <MyCalendar />
+    </div>
+  );
+}
+
+export default App;
